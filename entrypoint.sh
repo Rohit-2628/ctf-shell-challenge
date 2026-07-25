64636485b1c4e9201820652209a933936905bf04
Rohit <https://github.com/Rohit-2628/cyberanzen-header-challenge.git>
2026-07-25 20:23:48 +0530

    Here goes anotheer update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# 1. Catch the dynamic flag and write it safely to a file
+# Catch the dynamic flag and write it safely
 echo "${FLAG:-CTF{DEFAULT_LOCAL_FLAG}}" > /home/ctfplayer/flag.txt
 
-# 2. Destroy the environment variable so players can't cheat by typing 'env'
+# Destroy the environment variable for security
 unset FLAG
 
-# 3. Start a raw TCP listener on port 1337 that serves a bash terminal
-exec socat TCP-LISTEN:1337,reuseaddr,fork EXEC:"/bin/bash -i",pty,stderr,sane
+# Serve the challenge script DIRECTLY over the network (no pty/bash required)
+exec socat TCP-LISTEN:1337,reuseaddr,fork EXEC:"/home/ctfplayer/challenge.sh",stderr
