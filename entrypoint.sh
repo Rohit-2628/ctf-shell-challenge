78da7477240fc94ef1e773851426a76f9f65e95d
Rohit <https://github.com/Rohit-2628/cyberanzen-header-challenge.git>
2026-07-25 19:45:35 +0530

    Fixed bash interactive mode for socat

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ echo "${FLAG:-CTF{DEFAULT_LOCAL_FLAG}}" > /home/ctfplayer/flag.txt
 unset FLAG
 
 # 3. Start a raw TCP listener on port 1337 that serves a bash terminal
-exec socat TCP-LISTEN:1337,reuseaddr,fork EXEC:"/bin/bash",stderr
+exec socat TCP-LISTEN:1337,reuseaddr,fork EXEC:"/bin/bash -i",pty,stderr,sane
